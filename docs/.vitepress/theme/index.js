79df11189cc59a98ecf494829b28fa2b3b1cec10
studentsning <2300382007@qq.com>
2025-01-08 16:58:45 +0800

    update ⭐ 去除掉分割线引入

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -6,13 +6,11 @@ import UpdatedTime from '../../components/UpdatedTime.vue';
 import WordCount from '../../components/WordCount.vue';
 import Home from '../../components/Home.vue';
 import Layout from '../../components/Layout.vue';
-import { ElDivider, ElTag, ElPagination } from 'element-plus';
+import { ElTag } from 'element-plus';
 // css
 import '@fortawesome/fontawesome-free/css/all.css';
 import '../style.css'; // 引入 Tailwind CSS
-import 'element-plus/es/components/divider/style/css';
 import 'element-plus/es/components/tag/style/css';
-import 'element-plus/es/components/pagination/style/css';
 
 export default {
     ...DefaultTheme,
@@ -21,8 +19,6 @@ export default {
         app.component('WordCount', WordCount);
         app.component('Home', Home);
         app.component('Layout', Layout);
-        app.component(ElDivider.name, ElDivider); // 注册组件
-        app.component(ElTag.name, ElTag); // 注册组件
-        app.component(ElPagination.name, ElPagination); // 注册组件
+        app.component(ElTag.name, ElTag); // 标签
     },
 };
